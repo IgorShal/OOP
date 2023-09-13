7c234c259816f92aa4601074a09115e9a2d55faa
IgorShal <i.shalygin@g.nsu.ru>
2023-09-13 13:38:05 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/org/example/Solution.java
+++ Task_1_1_1/src/main/java/org/example/Solution.java
@@ -1,22 +1,29 @@
 
 package org.example;
-
+/**
+ * Класс решения
+ */
 
 public class Solution {
-    //Класс решения
+    /**
+     * Функция для смены элементов массива по индексам
+     */
+
     public static void swap(int index1, int index2, int[] arr) {
-        //Функция для смены элементов массива по индексам
+        //
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
-
+    /**
+     * Фуенкция для прохода по куче с просеиванием
+     */
     public static void heapify(int[] arr, int length, int i) {
-        //Фуенкция для прохода по куче с просеиванием
+
         int mx = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        if (left < length && arr[left] > arr[mx]){
+        if (left < length && arr[left] > arr[mx]) {
             mx = left;
         }
         if (right < length && arr[right] > arr[mx]) {
@@ -27,18 +34,22 @@ public class Solution {
             heapify(arr, length, mx);
         }
     }
-
+    /**
+     * Функция самого heap sort в которой мы сначала создаём из исходного массива кучу
+     * а потом вытесняем максимальные элементы
+     */
     public static String heapsort(int[] arr) {
-        //Функция самого heap sort в которой мы сначала создаём из исходного массива кучу а потом вытесняем максимальные элементы
+
         int length = arr.length;
-        for (int i = length / 2 - 1; i >= 0; i--){
+        for (int i = length / 2 - 1; i >= 0; i--) {
             heapify(arr, arr.length, i);
         }
         for (int i = length - 1; i >= 0; i--) {
             swap(0, i, arr);
             heapify(arr, i, 0);
         }
-        //Возвращаем результат как строку
+
+        // Возвращаем результат как строку
         String result = "[";
         for (int i = 0; i < length - 1; i++) {
             result = result.concat(arr[i] + ", ");
@@ -46,7 +57,9 @@ public class Solution {
         result += (arr[length - 1] + "]");
         return result;
     }
-
+    /**
+     * main функция
+     */
     public static void main(String[] args) {
         heapsort(new int[]{5, 4, 3, 2, 1});
     }
