0e5f6856e4aa5c2111df17b0c7854c49e75c25c5
IgorShal <i.shalygin@g.nsu.ru>
2023-09-13 11:36:57 +0700

    FFirst task

--- Task_1_1_1/src/main/java/org/example/Solution.java
+++ Task_1_1_1/src/main/java/org/example/Solution.java
@@ -3,39 +3,42 @@ package org.example;
 
 
 public class Solution {
-    public static void swap(int index1,int index2, int[] arr) {
+    public static void swap(int index1, int index2, int[] arr) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
-    public static void heapify(int[] arr,int length,int i){
+
+    public static void heapify(int[] arr, int length, int i) {
         int mx = i;
-        int left = 2*i +1;
-        int right = 2*i +2;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
         if (left < length && arr[left] > arr[mx])
             mx = left;
         if (right < length && arr[right] > arr[mx])
             mx = right;
-        if (mx != i){
-            swap(i,mx,arr);
-            heapify(arr,length,mx);
+        if (mx != i) {
+            swap(i, mx, arr);
+            heapify(arr, length, mx);
         }
     }
-    public static String heapsort(int[] arr){
+
+    public static String heapsort(int[] arr) {
         int length = arr.length;
-        for (int i =length/2-1;i>=0;i--)
-            heapify(arr,arr.length,i);
-        for (int i = length-1;i>=0;i--) {
+        for (int i = length / 2 - 1; i >= 0; i--)
+            heapify(arr, arr.length, i);
+        for (int i = length - 1; i >= 0; i--) {
             swap(0, i, arr);
-            heapify(arr,i,0);
+            heapify(arr, i, 0);
         }
         String result = "[";
-        for (int i = 0;i<length-1;i++)
+        for (int i = 0; i < length - 1; i++)
             result = result.concat(arr[i] + ", ");
-        result+=(arr[length-1] + "]");
+        result += (arr[length - 1] + "]");
         return result;
     }
+
     public static void main(String[] args) {
-        heapsort(new int[] {5, 4, 3, 2, 1});
+        heapsort(new int[]{5, 4, 3, 2, 1});
     }
 }
--- Task_1_1_1/src/test/java/SampleTest.java
+++ Task_1_1_1/src/test/java/SampleTest.java
@@ -3,31 +3,36 @@ import org.junit.jupiter.api.Assertions;
 
 public class SampleTest {
     @org.junit.jupiter.api.Test
-    void test1(){
+    void test1() {
         Assertions.assertEquals("[1, 2, 3, 4, 5]", Solution.heapsort(new int[]{5, 4, 3, 2, 1}));
     }
+
     @org.junit.jupiter.api.Test
-    void test2(){
+    void test2() {
         Assertions.assertNotEquals("[1, 1, 3, 4, 5]", Solution.heapsort(new int[]{1, 4, 3, 2, 1}));
     }
+
     @org.junit.jupiter.api.Test
-    void test3(){
+    void test3() {
         Assertions.assertEquals("[1, 1, 1, 1, 1]", Solution.heapsort(new int[]{1, 1, 1, 1, 1}));
     }
+
     @org.junit.jupiter.api.Test
-    void test4(){
+    void test4() {
         Assertions.assertNotEquals("[1, 4, 3, 4, 5]", Solution.heapsort(new int[]{5, 4, 3, 2, 1}));
     }
+
     @org.junit.jupiter.api.Test
-    void test5(){
+    void test5() {
         int[] arr = {5, 4};
-        Solution.swap(0,1,arr);
+        Solution.swap(0, 1, arr);
         Assertions.assertTrue(arr[0] == 4 && arr[1] == 5);
     }
+
     @org.junit.jupiter.api.Test
-    void test6(){
-        int[] arr = {1,2,3};
-        Solution.heapify(arr,3,0);
+    void test6() {
+        int[] arr = {1, 2, 3};
+        Solution.heapify(arr, 3, 0);
         Assertions.assertArrayEquals(arr, new int[]{3, 2, 1});
     }
 }
