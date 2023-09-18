dc7cc771354fc38a40ae4cbb5cc18b47a8624d42
IgorShal <i.shalygin@g.nsu.ru>
2023-09-18 19:30:15 +0700

    task_1_1_2

--- Task_1_1_2/src/test/java/SampleTest.java
+++ Task_1_1_2/src/test/java/SampleTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.api.Assertions;
  */
 public class SampleTest {
     @org.junit.jupiter.api.Test
-    void SumTest() {
+    void sumTest() {
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.plus(p2).equal(new Polynomial(new int[]{7, 14, 5, 7})));
     }
 
     @org.junit.jupiter.api.Test
-    void MinusTest() {
+    void minusTest() {
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.minus(p2).equal(new Polynomial(new int[]{7, -2, 1, 1})));
     }
 
     @org.junit.jupiter.api.Test
-    void MultTest() {
+    void multTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Polynomial p2 = new Polynomial(new int[]{1, -1});
         Assertions.assertTrue(p1.times(p2).equal(new Polynomial(new int[]{1, 0, 0, -1})));
