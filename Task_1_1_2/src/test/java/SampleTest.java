3e49080d941fefa5d10024cc3bd3fb6e5ad211d8
IgorShal <i.shalygin@g.nsu.ru>
2023-09-18 19:26:56 +0700

    task_1_1_2

--- Task_1_1_2/src/test/java/SampleTest.java
+++ Task_1_1_2/src/test/java/SampleTest.java
@@ -6,64 +6,58 @@ import org.junit.jupiter.api.Assertions;
  */
 public class SampleTest {
     @org.junit.jupiter.api.Test
-    void test1() {
+    void SumTest() {
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.plus(p2).equal(new Polynomial(new int[]{7, 14, 5, 7})));
     }
 
     @org.junit.jupiter.api.Test
-    void test2() {
+    void MinusTest() {
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.minus(p2).equal(new Polynomial(new int[]{7, -2, 1, 1})));
     }
 
     @org.junit.jupiter.api.Test
-    void test3() {
+    void MultTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Polynomial p2 = new Polynomial(new int[]{1, -1});
         Assertions.assertTrue(p1.times(p2).equal(new Polynomial(new int[]{1, 0, 0, -1})));
     }
 
     @org.junit.jupiter.api.Test
-    void test4() {
+    void toStringTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Polynomial p2 = new Polynomial(new int[]{1, -1});
         Assertions.assertEquals(p1.times(p2).toString(), "x^3 - 1.0");
     }
 
     @org.junit.jupiter.api.Test
-    void test5() {
+    void differenciateTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Assertions.assertEquals(p1.differentiate(1).toString(), "2.0x + 1.0");
     }
 
     @org.junit.jupiter.api.Test
-    void test6() {
+    void doubleDifferenciateTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Assertions.assertEquals(p1.differentiate(2).toString(), "2.0");
     }
 
     @org.junit.jupiter.api.Test
-    void test7() {
+    void verybigcountDifferenciate() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Assertions.assertEquals(p1.differentiate(50).toString(), "0.0");
     }
 
     @org.junit.jupiter.api.Test
-    void test8() {
-        Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
-        Assertions.assertEquals(p1.differentiate(50).toString(), "0.0");
-    }
-
-    @org.junit.jupiter.api.Test
-    void test9() {
+    void mainTest() {
         Polynomial.main(new String[]{null});
     }
 
     @org.junit.jupiter.api.Test
-    void test10() {
+    void evaluateTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Assertions.assertEquals(p1.evaluate(2), 7);
     }
