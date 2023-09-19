83fd280b3e4444e2d8044bb6b172c77d7cc83a58
IgorShal <i.shalygin@g.nsu.ru>
2023-09-19 10:05:25 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/org/example/Solution.java
+++ Task_1_1_1/src/main/java/org/example/Solution.java
@@ -69,6 +69,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        heapsort(new int[]{5, 4, 3, 2, 1});
+        System.out.println(heapsort(new int[]{5, 4, 3, 2, 1}));
     }
 }
