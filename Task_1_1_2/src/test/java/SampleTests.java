4c7eff5ba777d2508fb12c4b2046b82ebf2e2750
IgorShal <i.shalygin@g.nsu.ru>
2023-09-21 15:34:13 +0700

    v:

--- Task_1_1_2/src/test/java/SampleTests.java
+++ Task_1_1_2/src/test/java/SampleTests.java
@@ -6,21 +6,25 @@ import org.junit.jupiter.api.Assertions;
  */
 public class SampleTests {
     @org.junit.jupiter.api.Test
+<<<<<<< HEAD:Task_1_1_2/src/test/java/SampleTests.java
     void summTest() {
+=======
+    void SumTest() {
+>>>>>>> parent of dc7cc77 (task_1_1_2):Task_1_1_2/src/test/java/SampleTest.java
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.plus(p2).equal(new Polynomial(new int[]{7, 14, 5, 7})));
     }
 
     @org.junit.jupiter.api.Test
-    void minusTest() {
+    void MinusTest() {
         Polynomial p1 = new Polynomial(new int[]{7, 6, 3, 4});
         Polynomial p2 = new Polynomial(new int[]{8, 2, 3});
         Assertions.assertTrue(p1.minus(p2).equal(new Polynomial(new int[]{7, -2, 1, 1})));
     }
 
     @org.junit.jupiter.api.Test
-    void multTest() {
+    void MultTest() {
         Polynomial p1 = new Polynomial(new int[]{1, 1, 1});
         Polynomial p2 = new Polynomial(new int[]{1, -1});
         Assertions.assertTrue(p1.times(p2).equal(new Polynomial(new int[]{1, 0, 0, -1})));
