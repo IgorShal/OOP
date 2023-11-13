ee1b2e725a91838df165e79eae361019d7cea6d7
IgorShal <i.shalygin@g.nsu.ru>
2023-11-13 13:24:24 +0700

    Requested changes

--- Task_1_3_1/src/test/java/Tests.java
+++ Task_1_3_1/src/test/java/Tests.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
 import org.example.Finder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
