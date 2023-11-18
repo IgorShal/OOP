2457528570ebdf86885ad0e5bfcffa04514b333a
IgorShal <i.shalygin@g.nsu.ru>
2023-11-18 23:48:08 +0700

    Second

--- Task_1_4_1/src/test/java/CreditBookTest.java
+++ Task_1_4_1/src/test/java/CreditBookTest.java
@@ -1,7 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import org.example.CreditBook;
 import org.junit.jupiter.api.BeforeEach;
