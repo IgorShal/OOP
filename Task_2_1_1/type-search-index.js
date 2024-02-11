8a93f157a04cd6ded729457cafb99453b39fa2dd
IgorShal <IgorShal@users.noreply.github.com>
2024-02-11 11:54:30 +0000

    Deploying to gh-pages from @ IgorShal/OOP@5f6a4f421263192fa77ce9bcac783633eadf2a77 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"ParallelSolver"},{"p":"org.example","l":"ParallelStreamSolver"},{"p":"org.example","l":"SequentualSolver"},{"p":"org.example","l":"Solver"}]
