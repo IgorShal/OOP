273267454e1362d6a0b82209885d667c3c085982
IgorShal <i.shalygin@g.nsu.ru>
2024-02-19 17:38:07 +0700

    ReviewDog 3.

--- Task_2_1_1/src/main/java/org/example/ParallelSolver.java
+++ Task_2_1_1/src/main/java/org/example/ParallelSolver.java
@@ -2,8 +2,8 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
